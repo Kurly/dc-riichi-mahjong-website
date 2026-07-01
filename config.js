1eeb40c42f2b905ef6d0fdf6c31fc597a8db78af
Kurly <kpittlem@gmail.com>
2026-07-01 14:33:47 -0400

    Update config.js to add second calendar ID for one-off events

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const DCRM_CONFIG = {
     // The ID for your Weekly Meetups Calendar
     WEEKLY_CALENDAR_ID: '30f8f2004fa338915dd4798b4307f8ec48f9b150ee3d5fbc10b3506b50e62979@group.calendar.google.com',
 
-    // The ID for your Special/One-Off Events Calendar
-    ONEOFF_CALENDAR_ID: 'YOUR_NEW_ONEOFF_CALENDAR_ID_HERE' 
+    // The ID for your Special/One-Off Events Calendar (Tournaments, Conventions, etc.)
+    ONEOFF_CALENDAR_ID: '8456daeb81e2fbb618f23e115068438be4f9c7da496d3b8ae2f6d201e97e4abf@group.calendar.google.com' 
 };
