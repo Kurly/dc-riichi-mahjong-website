edcde55ea37a68d94d0d42ee1b247267a80f3070
Kurly <kpittlem@gmail.com>
2026-07-01 14:42:44 -0400

    Update calendar IDs in config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const DCRM_CONFIG = {
     API_KEY: 'AIzaSyDRsLC03k5ILWAaefOlj_2q-9OW99CgdJc', 
 
     // The ID for your Weekly Meetups Calendar
-    WEEKLY_CALENDAR_ID: '30f8f2004fa338915dd4798b4307f8ec48f9b150ee3d5fbc10b3506b50e62979@group.calendar.google.com',
-
+    WEEKLY_CALENDAR_ID: '8456daeb81e2fbb618f23e115068438be4f9c7da496d3b8ae2f6d201e97e4abf@group.calendar.google.com',
+    
     // The ID for your Special/One-Off Events Calendar (Tournaments, Conventions, etc.)
-    ONEOFF_CALENDAR_ID: '8456daeb81e2fbb618f23e115068438be4f9c7da496d3b8ae2f6d201e97e4abf@group.calendar.google.com' 
+    ONEOFF_CALENDAR_ID: '30f8f2004fa338915dd4798b4307f8ec48f9b150ee3d5fbc10b3506b50e62979@group.calendar.google.com' 
 };
